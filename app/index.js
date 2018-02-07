ecf1d9a419cbb2255353fe9f4e9460c3747408f5
Gokmen Goksel <gokmen@goksel.me>
2018-02-07 00:28:20 -0800

    app: update app definition for latest yeoman

--- app/index.js
+++ app/index.js
@@ -1,86 +1,79 @@
-'use strict';
+'use strict'
 
-var chalk = require('chalk');
-var yeoman = require('yeoman-generator');
-var _s = require('underscore.string');
+const chalk = require('chalk')
+const Generator = require('yeoman-generator')
+const _s = require('underscore.string')
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = class KDGenerator extends Generator {
+  constructor(args, opts) {
+    super(args, opts)
 
-  constructor: function () {
-    yeoman.generators.Base.apply(this, arguments);
-    this.argument('_appname', { type: String, required: false });
-  },
+    this.option('skip-install')
+    this.argument('_appname', { type: String, required: false })
+  }
 
-  _generate: function () {
+  prompting() {
+    console.log(require('yeoman-welcome'))
 
-    console.log('');
+    return this.prompt({
+      name: 'appname',
+      message: 'What is the name of your awesome kd.js project ?',
+      default: this.appname.replace(/\s/g, '-'),
+      filter: function(val) {
+        return _s.slugify(val)
+      },
+    }).then(props => {
+      this.appname = props.appname
+    })
+  }
 
-    var cb = this.async();
+  writing() {
+    console.log('')
 
-    var tpl = {
+    let cb = this.async()
+
+    let tpl = {
       appname: this.appname,
       name: this.user.git.name(),
       email: this.user.git.email(),
-    };
+    }
 
-    var mv = function (from, to) {
-      this.fs.move(this.destinationPath(from), this.destinationPath(to));
-    }.bind(this);
+    let mv = function(from, to) {
+      this.fs.move(this.destinationPath(from), this.destinationPath(to))
+    }.bind(this)
 
     this.fs.copyTpl(
       this.templatePath() + '/**/!(*.png|*.ico)', // ignore images
       this.destinationPath(),
       tpl
-    );
+    )
 
     this.fs.copy(
       this.templatePath() + '/app/images',
       this.destinationPath() + '/app/images'
-    );
+    )
 
-    mv('editorconfig', '.editorconfig');
-    mv('gitattributes', '.gitattributes');
-    mv('gitignore', '.gitignore');
-    mv('_package.json', 'package.json');
+    mv('editorconfig', '.editorconfig')
+    mv('gitattributes', '.gitattributes')
+    mv('gitignore', '.gitignore')
+    mv('_package.json', 'package.json')
 
-    cb();
+    cb()
+  }
 
-  },
-
-  init: function () {
-
-    console.log(require('yeoman-welcome'));
-
-    if (this._appname) {
-      this.appname = _s.slugify(this._appname);
-      this._generate();
-    }
-    else {
-      var cb = this.async();
-      this.prompt({
-        name: 'appname',
-        message: 'What is the name of your awesome kd.js project ?',
-        default: this.appname.replace(/\s/g, '-'),
-        filter: function (val) {
-          return _s.slugify(val);
-        }
-      }, function (props) {
-        this.appname = props.appname;
-        this._generate();
-      }.bind(this));
-    }
-  },
-
-  install: function () {
+  install() {
+    if (this.options['skip-install']) return
     this.installDependencies({
       bower: false,
       callback: function() {
-        console.log (
-          '\nEverything is ready, run '+ chalk.yellow.bold('gulp') +
-          ' to see ' + chalk.green.bold(this.appname) + ' alive!\n'
-        );
-      }.bind(this)
-    });
+        console.log(
+          '\nEverything is ready, run ' +
+            chalk.yellow.bold('gulp') +
+            ' to see ' +
+            chalk.green.bold(this.appname) +
+            ' alive!\n'
+        )
+      }.bind(this),
+    })
   }
-
-});
+}
